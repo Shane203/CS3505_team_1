39038c7366059fa5a316289b1107638306e3104a
Javed6234 <32278192+Javed6234@users.noreply.github.com>
2018-02-20 19:53:06 +0000

    changes to test

---
+++ test_client.py
@@ -0,0 +1,18 @@
+import unittest
+from client import Ludo
+serv = __import__('server-1p.py')
+
+class TestClient(unittest.TestCase):
+    def setUp(self):
+        serv()
+        ludo = Ludo()
+        ludo.setup()
+        
+
+    def test_ludo(self):
+        print("hi")
+        ludo.run()
+        
+if __name__ == '__main__':
+    unittest.setUp()
+    unittest.test_ludo()
