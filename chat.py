bfa0e168db26d9edc03bd5a4e31f5e6ec43eb76b
crickash <36648780+crickash@users.noreply.github.com>
2018-03-04 17:12:08 +0000

    Add files via upload
    chat program

---
+++ chat.py
@@ -0,0 +1,60 @@
+import _thread
+import json
+from socket import *
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+
+class ChatBox():
+    def __init__(self,sock):
+        self.name = "" 
+        self.sock = sock
+
+
+        
+
+    def new_message(self,msg):
+        string = msg["Name"] + ": " + msg["msg"]
+        self.recvd.insert("end","%s \n"%string)
+        self.recvd.see(END)
+
+    
+    def send(self,event):
+        message = {"Name":self.name,"msg":self.msg.get()}
+        message = json.dumps(message)
+        self.sock.sendall(message.encode())
+        self.msg.delete(0, 'end')
+
+        
+    def start(self,name):
+        self.name = name
+        self.root = Tk()
+        self.frame = Frame(self.root)
+        self.msg = Entry(self.frame, width=50, font=('TkDefaultFont', 11)) #Where the Client enters the message
+        self.msg.configure(bg='pink')
+        self.recvd =ScrolledText(self.frame,height=30,width=50) #The text area where all received messages go.
+        self.recvd.configure(bg='lightblue')
+        self.root.title("Chat")
+        self.root.minsize(500,500)
+        self.root.maxsize(500,500)
+        self.root.configure(bg='lightblue')
+        self.root.bind('<Return>',self.send)
+        self.recvd.pack()
+        self.msg.pack(ipady=5)
+        self.frame.pack()
+        self.root.mainloop()
+        
+
+"""sock = socket(AF_INET, SOCK_STREAM)#Creates a TCP server socket.
+server_address =(gethostbyname(gethostname()), 11000) #Sets values for host- the current domain name and port number 10000.
+ipaddr = gethostbyname(gethostname())
+sock.connect(server_address)
+Game  = ChatBox(sock)
+_thread.start_new_thread(Game,())
+while True:
+    pass
+
+"""
+       
+
+
+
