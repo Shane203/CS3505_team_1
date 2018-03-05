e996d50da630644b1c5722ca97bda117bf321e4c
Shane Houston <shane.houston1997@gmail.com>
2018-03-05 15:44:12 +0000

    Update player.py

--- player.py
+++ player.py
@@ -1,4 +1,17 @@
 class Player(object):
+    """Create an object to represent a player with 4 piece objects.
+    
+    The function of this class is limited to representing attributes for the player;
+    therefore there are no methods. This class also holds universal attributes, like 
+    all_pieces and names that are used to draw pieces and scores that don't belong to
+    this client and therefore wouldn't have access to.
+    
+    Args:
+        colour: The colour used to represent the player and all their pieces.
+        name: The name displayed with this player's score and chat messages.
+        all_pieces: A list of all Piece objects in the game currently.
+        names: The list of all player names. Needed to display the score board.
+    """
     def __init__(self, colour, name, all_pieces, names):
         self.colour = colour
         self.name = name
@@ -18,19 +31,19 @@ class Player(object):
         if self.colour == "red":
             self.start = 0
             self.end = 51
-            self.low_range = 0  # Is used for move_piece function
+            self.low_range = 0  # Is used for the Board.move_piece function
         elif self.colour == "green":
             self.start = 13
             self.end = 11
-            self.low_range = 4  # Is used for move_piece function
+            self.low_range = 4  # Is used for the Board.move_piece function
         elif self.colour == "yellow":
             self.start = 26
             self.end = 24
-            self.low_range = 8  # Is used for move_piece function
+            self.low_range = 8  # Is used for the Board.move_piece function
         elif self.colour == "blue":
             self.start = 39
             self.end = 37
-            self.low_range = 12  # Is used for move_piece function
+            self.low_range = 12  # Is used for the Board.move_piece function
         self.my_pieces = []
         for piece in self.ALL_PIECES:
             if self.colour == piece.colour:
