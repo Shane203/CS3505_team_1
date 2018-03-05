b42fac040097710b21b0866d21c02a2dfb5540f6
davidcussen96 <36636401+davidcussen96@users.noreply.github.com>
2018-03-05 15:44:44 +0000

    Update dice.py

--- dice.py
+++ dice.py
@@ -10,8 +10,8 @@ class Dice():
         self.connection = connection
 
     def dice_rule(self):
-        """
-        Checks if player can roll a dice
+        """Checks if player can roll a dice.
+        
         :var self.connection.my_player.roll: Checks previous value of dice/roll
         :var self.connection.my_player.special_move: Checks if player had piece
         land on opposing player's piece
@@ -41,22 +41,48 @@ class Dice():
             return False
 
     def roll_dice(self):
+        """If the player has the turn token, dice roll token and the player can 
+        roll the dice then roll the dice.
+        
+        :var self.connection.my_player.turn_token: Checks if it is the player's turn.
+        :var self.connection.my_player.diceroll_token: Checks if it is the 
+        player's turn to roll the dice.
+        :var self.dice_rule(): Returns True if the player can roll the dice.
+        :var self.check_for_bias(): Introduces bias. It is more likely to roll a 6 if 
+        you've no pieces on the board.
+        :var self.connection.my_player.colour: The player's colour.
+        """
         if self.connection.my_player.turn_token and \
                 self.connection.my_player.diceroll_token and self.dice_rule():
             # Prevents roll until piece moved
             self.connection.my_player.diceroll_token = False
             bias = self.check_for_bias()
+            #Constructs a msg to send to the server.
             msg = {"Colour": self.connection.my_player.colour,
                    "roll": True, "bias": bias}
+            # Plays the sound for the dice roll
             pygame.mixer.Sound.play(ROLL_DICE)
+            #Sends the 'msg' to the server.
             data = json.dumps(msg)
             self.connection.sock.sendall(data.encode())
+            #Generates the roll dice animation.
             self.roll_dice_gif(1, 1, 900, 230)
 
     def check_for_bias(self):
+        """Checks for bias in the player's pieces. This happens if the player has 
+        no pieces on the board.
+        
+        :var self.connection.my_player.my_pieces: list of the player's pieces objects.
+        :var piece.get_position(): position of piece on the board.
+        :return: True if all pieces are at home (introduce bias) or False if at least 
+        one piece is on the board (no bias).
+        """
         for piece in self.connection.my_player.my_pieces:
+            #Iterates through the player's pieces.
             if piece.get_position() is not None:
+                #If at least one piece is outside/not None then return False
                 return False
+        #All pieces are at home.
         return True
 
     def roll_dice_gif(self, n, in_, x, y):
@@ -81,4 +107,10 @@ class Dice():
             i += 1
 
     def display_dice(self, x, y, img):
+        """Displays the dice image 'img' at position (x,y) on the SCREEN.
+        
+        :param x: x coordinate of dice image.
+        :param y: y coorinate of dice image.
+        :param img: url of the dice image
+        """
         SCREEN.blit(img, (x, y))
