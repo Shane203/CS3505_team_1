2f62d59b8dec84fd2e6fcd0fc8531efcf81d2dae
Aidan <112433108@umail.ucc.ie>
2018-03-06 11:38:28 +0000

    update comments chat.py

--- box_and_button.py
+++ box_and_button.py
@@ -2,18 +2,23 @@ import pygame
 from constants import SCREEN
 from constants import BLACK
 pygame.init()
+
+
 class Box(object):
-    
+    """
+    This class is used to produce boxes to represent information to players
+
+    Args:
+        :arg msg: Message to appear in the box.
+        :arg x: x coordinate of the top left-hand corner of the box.
+        :arg y: y coordinate of the top left-hand corner of the box.
+        :arg w: width of the box.
+        :arg h: height of the box.
+        :arg c: colour of the box
+        :arg s: size of the border line of the box, default 0.
+
+    """
     def __init__(self, msg, x, y, w, h, c, s=0):
-        """Initialisation.
-        :param msg: Message to appear in the box.
-        :param x: x coordinate of the top left-hand corner of the box.
-        :param y: y coordinate of the top left-hand corner of the box.
-        :param w: width of the box.
-        :param h: height of the box.
-        :param c: colour of the box
-        :param s: size of the border line of the box, default 0.
-        """
         self._msg = msg
         self._x = x
         self._y = y
@@ -26,10 +31,13 @@ class Box(object):
         """Renders the msg displayed in the box.
         
         :param text: text to be rendered on the surface.
+        :type text: str
         :param font: font of the text.
-        :return text_surface, text_surface.get_rect(): "text_surface" is the surface 
-        the text is to be displayed on. "text_surface.get_rect()" is the rectangle 
-        containing the surface with the text.
+        :type font: str
+        :return text_surface: ``text_surface`` is the surface the text
+                to be displayed on.
+        :return text_surface.get_rect(): ``text_surface.get_rect()`` is the
+                rectangle containing the surface with the text.
         """
         text_surface = font.render(text, True, BLACK)
         return text_surface, text_surface.get_rect()
@@ -38,11 +46,12 @@ class Box(object):
         """Sets the box's message.
         
         :param msg: Message to be displayed in the box.
+        :type msg: str
         """
         self._msg = msg
 
     def draw(self):
-        """Draws the box."""
+        """Draws the box which will display information to the player."""
         pygame.draw.rect(SCREEN, self._c, (self._x, self._y, self._w, self._h),
                          self._s)
         small_text = pygame.font.Font("freesansbold.ttf", 20)
@@ -50,26 +59,27 @@ class Box(object):
         text_rect.center = ((self._x + (self._w/2)), (self._y+(self._h/2)))
         SCREEN.blit(text_surf, text_rect)
 
+
 class Button(Box):
     """
     Button class inherits from the box class.
+
+    Args:
+        :arg ac: active colour. The colour of the box when the mouse hovers
+                   over it.
+        :arg action: method called when the button is clicked, default = None.
     """
     def __init__(self, msg, x, y, w, h, c, s, ac, action=None):
-        """Initialisation.
-        
-        :param ac: active colour. The colour of the box when the mouse hovers over it.
-        :param action: method called when the button is clicked, default = None.
-        """
         Box.__init__(self, msg, x, y, w, h, c, s)
         self._ac = ac
         self._action = action
 
     def draw(self):
-        """Draws the button. If the mouse is within the boundaries of the button it changes
-        to its active colour 'ac'.
-        
-        :var mouse: Is the location of the mouse [x, y].
+        """Draws the button. If the mouse is within the boundaries of the button
+        it changes to its active colour 'ac'.
+
         """
+        # This is the location of the mouse [x, y].
         mouse = pygame.mouse.get_pos()
         if self._x + self._w > mouse[0] > self._x and \
            self._y + self._h > mouse[1] > self._y:
@@ -85,16 +95,17 @@ class Button(Box):
 
     def click(self):
         """Determines whether the button has been clicked.
-        
-        :var mouse: Is the location of the mouse [x, y].
-        :var click: Is the location of the click. x coordinate, y coordinate 
-        and whether it was the right or left press [x, y, r/h]
-        :return None if the button hasnt been pressed and calls the function
-        'action' if it has been pressed.
+
+        :return: None if the button hasn't been pressed and calls the function
+                'action' if it has been pressed.
+
         """
-        mouse = pygame.mouse.get_pos()
+        mouse = pygame.mouse.get_pos()  # Is the location of the mouse [x, y]
+        # click: Is the location of the click.x coordinate, y coordinate and
+        # whether it was the right or left press [x, y, r/h]
         click = pygame.mouse.get_pressed()
-        if self._x + self._w > mouse[0] > self._x and self._y + self._h > mouse[1] > self._y:
-            if click[0] == 1 and self._action != None:
+        if self._x + self._w > mouse[0] > self._x and self._y + self._h > \
+                mouse[1] > self._y:
+            if click[0] == 1 and self._action is not None:
                 return self._action()
         return None
--- chat.py
+++ chat.py
@@ -4,62 +4,73 @@ from socket import *
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
 
+
 class ChatBox():
     """
     Creates a chat object/window.
-    It allows mutiple playes to chat with each other if they wish to.
+    It allows multiple players to chat with each other if they wish to.
     
-    :param sock: This is the socket through which messages will be sent/recieved.
+    :param sock: This is the socket through which messages will be
+           sent/received.
     :type sock: python socket
     """
-    def __init__(self,sock):
+    def __init__(self, sock):
         self.name = ""  # Initially set to empty string
-        self.sock = sock # set socket to a socket already being used
+        self.sock = sock  # set socket to a socket already being used
         
-    def new_message(self,msg):
+    def new_message(self, msg):
         """
-        When a message is recieved this will show it in the chat window 
+        When a message is received this will show it in the chat window.
 
-        :param msg: New message recieved from other player
+        :param msg: New message received from other player
         :type msg: String.
         """
-        string = msg["Name"] + ": " + msg["msg"] # concatenate name + message
-        self.recvd.insert("end","%s \n"%string) # shows the message on the screen
-        self.recvd.see(END) # always shows last line recieved
+        string = msg["Name"] + ": " + msg["msg"]  # concatenate name + message
+        # shows the message on the screen
+        self.recvd.insert("end", "%s \n" % string)
+        self.recvd.see(END)  # always shows last line received
     
-    def send(self,event):
-         """ 
-        sends a json message to server when you press "Return" key to send a message 
+    def send(self, event):
+        """
+        Sends a json message to server when you press "Return" key to send a
+        message.
 
         :param event: Gets called when an event happens (Return Key pressed)
         :type event: Key Press Event.
         """
-        if self.msg.get().strip() != "": # validates empty string
-            message = {"Name":self.name,"msg":self.msg.get()} # sets message to be sent
-            message = json.dumps(message) # converts it in json form
-            self.sock.sendall(message.encode()) # sends message to server
-            self.msg.delete(0, 'end') # clear the variable
+        if self.msg.get().strip() != "":  # validates empty string
+            # sets message to be sent
+            message = {"Name": self.name, "msg": self.msg.get()}
+            message = json.dumps(message)  # converts it in json form
+            self.sock.sendall(message.encode())  # sends message to server
+            self.msg.delete(0, 'end')  # clear the variable
         
-    def start(self,name):
+    def start(self, name):
         """ 
-        creates a tkinter window where a person can send/recieve messages from others 
+        creates a tkinter window where a person can send/receive messages
+        from others.
 
         :param name: Name of player (self)
         :type name: String.
         """
-        self.name = name # set name to the name entered in form
-        self.root = Tk() # creates tkinter window
-        self.frame = Frame(self.root) # create a frame on the window
-        self.msg = Entry(self.frame, width=50, font=('TkDefaultFont', 11)) #Where the Client enters the message
-        self.msg.configure(bg='pink') # set backgound colour of textfield
-        self.recvd =ScrolledText(self.frame,height=30,width=50) #The text area where all received messages go.
-        self.recvd.configure(bg='lightblue') # set background colour of recieved messages
-        self.root.title("Chat") # set title of window to chat
-        self.root.minsize(500,500) # minimum size of tkinter window
-        self.root.maxsize(500,500) # maximum size of tkinter window
-        self.root.configure(bg='lightblue') # set background of tkinter window
-        self.root.bind('<Return>',self.send) # listens for return key press
-        self.recvd.pack() # packs message box
-        self.msg.pack(ipady=5) # packs textfield
-        self.frame.pack() # packs frame
-        self.root.mainloop() # loop for showing new message
+        self.name = name  # set name to the name entered in form
+        self.root = Tk()  # creates tkinter window
+        # Create a frame on the window
+        self.frame = Frame(self.root)
+        # Where the Client enters the message
+        self.msg = Entry(self.frame, width=50, font=('TkDefaultFont', 11))
+        # Set background colour of textfield
+        self.msg.configure(bg='pink')
+        # The text area where all received messages go.
+        self.recvd = ScrolledText(self.frame, height=30, width=50)
+        # set background colour of received messages
+        self.recvd.configure(bg='lightblue')
+        self.root.title("Chat")  # set title of window to chat
+        self.root.minsize(500, 500)  # minimum size of tkinter window
+        self.root.maxsize(500, 500)  # maximum size of tkinter window
+        self.root.configure(bg='lightblue')  # set background of tkinter window
+        self.root.bind('<Return>', self.send)  # listens for return key press
+        self.recvd.pack()  # packs message box
+        self.msg.pack(ipady=5)  # packs textfield
+        self.frame.pack()  # packs frame
+        self.root.mainloop()  # loop for showing new message
